d72faba426ba4af0c249decdc1b35ba35ad4a6df
MarcosRaiCunha <marcosraicunha@gmail.com>
2019-12-20 09:26:40 -0300

    Primeiro Projeto

---
+++ projetos/listaDeCompras/listaDeComprasSession.js
@@ -0,0 +1,33 @@
+const env = require('../../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const session = require('telegraf/session')
+const bot = new Telegraf(env.token)
+
+
+const gerarBotoes = () => Extra.markup(
+  Markup.inlineKeyboard(
+    lista.map(item => Markup.callbackButton(item, `delete ${item}`)),
+    { columns: 3 }
+  )
+)
+
+bot.start(async ctx => {
+  const name = ctx.update.message.from.first_name
+  await ctx.reply(`Seja bem vindo ${name}!`)
+  await ctx.reply(`Escreva os itens que deseja adicionar...`)
+  ctx.session.lista = []
+})
+
+bot.on('text', ctx => {
+  ctx.session.lista.push(ctx.update.message.text)
+  ctx.reply(`${ctx.update.message.text} adicionado!`, gerarBotoes())
+})
+
+bot.action(/delete (.+)/, ctx => {
+  ctx.session.lista = ctx.session.lista.filter(item => item !== ctx.match[1])
+  ctx.reply(`${ctx.match[1]} deletado!`, gerarBotoes())
+})
+
+bot.startPolling()
